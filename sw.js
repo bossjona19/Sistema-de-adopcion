7267e90dcfd0048e3984c02530ae23d359a1df50
Bosjona19 <quinterojonathan108@gmail.com>
2026-05-25 14:12:06 -0500

    version1.0

--- sw.js
+++ sw.js
@@ -46,16 +46,17 @@ const STATIC = [
 ];
 
 self.addEventListener('install', ev => {
+  self.skipWaiting();
   ev.waitUntil(
-    caches.open(CACHE).then(c => c.addAll(STATIC)).then(() => self.skipWaiting())
+    caches.open(CACHE).then(c => c.addAll(STATIC))
   );
 });
 
 self.addEventListener('activate', ev => {
+  clients.claim();
   ev.waitUntil(
     caches.keys()
       .then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
-      .then(() => self.clients.claim())
   );
 });
 
